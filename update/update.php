51e8f3a47c89b0407e85707f2f601d1ef88e104f
William Stam <awstam@gmail.com>
2015-01-29 12:12:31 +0200

    latest

--- update/update.php
+++ update/update.php
@@ -37,14 +37,10 @@ class update {
 	//	$str = str_replace(".git","",$cfg['git']['path']);
 	//	$output = str_replace("From $str","", $output);
 		$output = str_replace("* branch            ". $cfg['git']['branch'] ."     -> FETCH_HEAD","", $output);
-		$output .= "</hr>";
+		$output .= "</hr>\n\n";
 		$output .= shell_exec('php composer.phar update');
 		
 		$return .= trim($output);
-
-
-
-
 
 		return $return;
 	}
