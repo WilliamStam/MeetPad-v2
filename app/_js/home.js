167403e4eec0e2dd2b03e6dae15f50f62930c1dc
William Stam <awstam@gmail.com>
2015-03-11 16:09:22 +0200

    latest

--- app/_js/home.js
+++ app/_js/home.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
 			
 		},
 		//Default is 75px, set to 0 for demo so any distance triggers swipe
-		threshold:0,
+		threshold:75,
 		allowPageScroll:"auto"
 	});
 	$("#left-area").swipe( {
