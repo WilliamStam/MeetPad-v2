2c02a3040373bfd40faefb07ccf7e5ec640cfc58
William Stam <awstam@gmail.com>
2015-05-06 11:19:41 +0200

    latest

--- app/_js/meeting.js
+++ app/_js/meeting.js
@@ -85,8 +85,13 @@ function getData() {
 		$("#loading-mask").fadeOut();
 
 
+		$.doTimeout(400,function(){
+			resize();
+			
+		})
 		resize()
-
+		$("#left-area-content .scroll-pane").data("jsp").scrollToElement("tr[data-id='805']",true,false);
+		showContent_state();
 		
 	});
 
@@ -95,10 +100,9 @@ function getData() {
 
 
 var settings = {
-	maintainPosition: true, // add your settings here, below are examples
+	maintainPosition: true,
 	arrowButtonSpeed: 1, 
 	showArrows: false, 
-	animateScroll: true
 };
 
 function resize() {
@@ -122,3 +126,47 @@ function resize() {
 
 
 }
+
+
+$(document).on('click', '#details-record-prev', function () {
+	prevRecord();
+});
+$(document).on('click', '#details-record-next', function () {
+	nextRecord();
+});
+
+
+function nextRecord() {
+	var ID = $.bbq.getState("ID");
+	var $item = $("#record-list .record[data-ID='" + ID + "']");
+	var $new = $item.nextAll("tr.record:first");
+	$.bbq.pushState({ID:$new.attr("data-ID")});
+	getData();
+}
+function prevRecord() {
+	var ID = $.bbq.getState("ID");
+	var $item = $("#record-list .record[data-ID='" + ID + "']");
+	var $new = $item.prevAll("tr.record:first");
+	$.bbq.pushState({ID:$new.attr("data-ID")});
+	getData();
+}
+
+function showContent_state() {
+	var ID = $.bbq.getState("ID");
+	if ($("#record-list .record[data-ID='" + ID + "']").prevAll("tr.record:first").length == 0) {
+		$("#details-record-prev").attr("disabled", "disabled");
+	} else {
+		$("#details-record-prev").removeAttr("disabled");
+	}
+	if ($("#record-list .record[data-ID='" + ID + "']").nextAll("tr.record:first").length == 0) {
+		$("#details-record-next").attr("disabled", "disabled");
+	} else {
+		$("#details-record-next").removeAttr("disabled");
+	}
+	var tab = $.bbq.getState("details-tab"), $details_modal = $('#ab-details-modal');
+	if (!tab) tab = "details-pane-details";
+	$('.nav-tabs li a[href="#' + tab + '"]', $details_modal).parent().addClass("active");
+	$('#' + tab + '', $details_modal).addClass("active");
+
+}
+
