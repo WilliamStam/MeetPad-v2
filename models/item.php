19b47f4369c1901dc82a42137ccbe403dc7156e8
William Stam <william@impreshin.com>
2015-07-06 14:40:59 +0200

    latest

--- models/item.php
+++ models/item.php
@@ -28,9 +28,13 @@ class item extends _ {
 			FROM mp_content
 			WHERE $where;
 		";
+		if ($userID===true){
+			$userID =  $this->user['global_admin']=='1'?false:$this->user['ID'];
+		}
+		
 
 		if ($userID) {
-			if ($userID===true)$userID = $this->user['ID'];
+			
 			$sql = "
 			SELECT DISTINCT  mp_content.*, mp_categories.category, (SELECT count(ID) FROM mp_content_comments WHERE contentID = mp_content.ID AND mp_content_comments.deleted is null) AS commentCount
 			FROM (((mp_content INNER JOIN mp_content_group ON mp_content.ID = mp_content_group.contentID) INNER JOIN mp_users_group ON mp_content_group.groupID = mp_users_group.groupID) INNER JOIN mp_categories ON mp_content.categoryID = mp_categories.ID) INNER JOIN mp_users_company ON mp_users_group.userID = mp_users_company.userID
